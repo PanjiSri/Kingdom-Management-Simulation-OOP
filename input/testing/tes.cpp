fe8dc3e2cf4fb4f8014b58473e1a73a071ced7b4
PanjiSri <neiabaraja04@gmail.com>
2024-04-12 17:21:25 +0700

    feat:grandmaster, fix: input

--- input/testing/tes.cpp
+++ input/testing/tes.cpp
@@ -3,15 +3,15 @@
 
 int main(){
 
-    list_hasil_baca a;
+    // list_hasil_baca a;
 
-    a.readPlanFromFile("../header/plant.txt");
-    for (int i = 0; i < a.getUkuran(); i++){    \
-        cout << a.getObjek(i).getId() << " ";
-        cout << a.getObjek(i).getKodeHuruf() << " ";
-        cout << a.getObjek(i).getName() << " " ;
-        cout << a.getObjek(i).getType() << " " ;
-        cout << a.getObjek(i).getDurationToHarvest() << " "; 
-        cout << a.getObjek(i).getPrice() << " " << endl;
-    }
+    // a.readPlanFromFile("../header/plant.txt");
+    // for (int i = 0; i < a.getUkuran(); i++){    \
+    //     cout << a.getObjek(i).getId() << " ";
+    //     cout << a.getObjek(i).getKodeHuruf() << " ";
+    //     cout << a.getObjek(i).getName() << " " ;
+    //     cout << a.getObjek(i).getType() << " " ;
+    //     cout << a.getObjek(i).getDurationToHarvest() << " "; 
+    //     cout << a.getObjek(i).getPrice() << " " << endl;
+    // }
 }
